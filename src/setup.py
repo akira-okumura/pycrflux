f84495903b242cd3f3e7eb19b7b57384ec4d6467
oxon <oxon>
2009-02-12 18:23:39 +0000

    Change comment for the file

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,12 @@
 """
+Setup script for PyCRFlux
+$Id: setup.py,v 1.2 2009/02/12 18:23:39 oxon Exp $
 """
-# $Id: setup.py,v 1.1 2009/02/12 18:10:39 oxon Exp $
 
 from distutils.core import setup
 
 setup(name="PyCRFlux",
-      version="1.0.0",
+      version="1.0.1",
       description="Package for cosmic-ray flux calculation",
       author="Akira Okumura",
       author_email="oxon@ceres.phys.s.u-tokyo.ac.jp",
