2d6a7920708656cf04fb2051fa9b56ab2eb05196
oxon <oxon>
2009-02-14 04:06:44 +0000

    Update version number to 1.0.3

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,12 @@
 """
 Setup script for PyCRFlux
-$Id: setup.py,v 1.3 2009/02/12 18:38:15 oxon Exp $
+$Id: setup.py,v 1.4 2009/02/14 04:06:44 oxon Exp $
 """
 
 from distutils.core import setup
 
 setup(name="PyCRFlux",
-      version="1.0.2",
+      version="1.0.3",
       description="Package for cosmic-ray flux calculation",
       author="Akira Okumura",
       author_email="oxon@ceres.phys.s.u-tokyo.ac.jp",
