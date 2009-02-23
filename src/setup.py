997451e782fb97b93cc77a39a664eed5372ff0ac
oxon <oxon>
2009-02-23 16:22:14 +0000

    - Change distutils to numpy.distutils so that FORTRAN code can be compiled - Update version number to 1.1.0

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,18 @@
 """
 Setup script for PyCRFlux
-$Id: setup.py,v 1.5 2009/02/14 07:11:27 oxon Exp $
+$Id: setup.py,v 1.6 2009/02/23 16:22:14 oxon Exp $
 """
 
-from distutils.core import setup
+from numpy.distutils.core import Extension, setup
+
+ext = Extension(name="cr_flux.pp_meson", sources=["ext/pp_meson_mod.f",])
 
 setup(name="PyCRFlux",
-      version="1.0.4",
+      version="1.1.0",
       description="Package for cosmic-ray flux calculation",
       author="Akira Okumura",
       author_email="oxon@ceres.phys.s.u-tokyo.ac.jp",
       packages=["cr_flux"],
-      package_data={"cr_flux": ["data/*/*.dat", "data/*/*.gz"]}
+      package_data={"cr_flux": ["data/*/*.dat", "data/*/*.gz"]},
+      ext_modules = [ext,]
       )
