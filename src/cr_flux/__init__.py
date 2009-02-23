83f0d24d5553fd9f86f167a9c5d234dc6888a663
oxon <oxon>
2009-02-23 15:56:17 +0000

    Add pp_meson import

--- src/cr_flux/__init__.py
+++ src/cr_flux/__init__.py
@@ -1,7 +1,8 @@
 """
 """
-#$Id: __init__.py,v 1.1 2009/02/12 18:07:42 oxon Exp $
+#$Id: __init__.py,v 1.2 2009/02/23 15:56:17 oxon Exp $
 
 from matter import *
+from pp_meson import *
 from pp_model import *
 from spectrum import *
