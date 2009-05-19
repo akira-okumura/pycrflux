3ca3a8d3f137ccd9798b85fdb9bc67f97a37a018
oxon <oxon>
2009-05-19 17:05:38 +0000

    Update the version number to 1.3.1

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 """
 Setup script for PyCRFlux
-$Id: setup.py,v 1.10 2009/05/17 09:32:34 oxon Exp $
+$Id: setup.py,v 1.11 2009/05/19 17:05:38 oxon Exp $
 """
 
 from numpy.distutils.core import Extension, setup
@@ -8,7 +8,7 @@ from numpy.distutils.core import Extension, setup
 ext = Extension(name="cr_flux.pp_meson", sources=["ext/pp_meson_mod.f",])
 
 setup(name="PyCRFlux",
-      version="1.3.0",
+      version="1.3.1",
       description="Package for cosmic-ray flux calculation",
       author="Akira Okumura",
       author_email="oxon@ceres.phys.s.u-tokyo.ac.jp",
